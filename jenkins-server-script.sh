616e9848750b36d8e9a38a49dc9be6150c65ff22
Araal Kaz <araalkaz@gmail.com>
2024-04-04 20:02:32 +0100

    This script installs jenkins, terraform,docker, git, unzip, openjdk-17-jre

---
+++ jenkins-server-script.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+## INSTALLING JAVA, JENKINS, GIT, UNZIP TERRAFORM AND DOCKER ##
+
+# Installing Java
+sudo apt update
+sudo apt install openjdk-17-jre -y
+
+# Installing Jenkins
+sudo wget -O /usr/share/keyrings/jenkins-keyring.asc https://pkg.jenkins.io/debian/jenkins.io-2023.key
+echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]" https://pkg.jenkins.io/debian binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt-get update
+sudo apt-get install jenkins -y
+sudo systemctl start jenkins
+sudo systemctl enable jenkins
+sudo cat /var/lib/jenkins/secrets/initialAdminPassword
+
+# Installing git and unzip
+sudo apt install git -y
+sudo apt install unzip -y
+
+## Installing Terraform
+# Ensure that your system is up to date and you have installed the gnupg, software-properties-common, 
+# and curl packages installed. You will use these packages to verify HashiCorp's GPG signature and install 
+# HashiCorp's Debian package repository.
+sudo apt-get update && sudo apt-get install -y gnupg software-properties-common
+# Install the HashiCorp GPG key.
+wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor | sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg > /dev/null
+# Verify the key's fingerprint.
+gpg --no-default-keyring --keyring /usr/share/keyrings/hashicorp-archive-keyring.gpg --fingerprint
+# Add the HashiCorp Linux repository.
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+# Download the package information from HashiCorp.
+sudo apt update
+# Install Terraform.
+sudo apt-get install terraform -y
+# Verify the installation.
+terraform --version
+
+
+# Installing Docker
+    # Add Docker's official GPG key:
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin docker-compose -y
+
+# Give user permission to use docker
+sudo usermod -aG docker $(whoami)
+newgrp docker
+sudo apt-get upgrade -y
+
+
